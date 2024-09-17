3957910bd3ccf0ffb86b10b27c9bf8f0aa92695e
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-17 16:44:04 +0630

    -

--- DotNet8.ConsoleApp.MongoDbExample/Program.cs
+++ DotNet8.ConsoleApp.MongoDbExample/Program.cs
@@ -1,13 +1,12 @@
-﻿namespace DotNet8.ConsoleApp.MongoDbExample
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            MongoDbService dbService = new();
-            await dbService.Run();
+﻿namespace DotNet8.ConsoleApp.MongoDbExample;
 
-            Console.ReadKey();
-        }
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        MongoDbService dbService = new();
+        await dbService.Run();
+
+        Console.ReadKey();
     }
 }
