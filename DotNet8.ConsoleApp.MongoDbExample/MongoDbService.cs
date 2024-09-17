c96aced913d975cc3777012aae47495b12e7bd9f
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-17 16:34:56 +0630

    [ Added ] UpdateBlogAsync

--- DotNet8.ConsoleApp.MongoDbExample/MongoDbService.cs
+++ DotNet8.ConsoleApp.MongoDbExample/MongoDbService.cs
@@ -12,25 +12,44 @@ namespace DotNet8.ConsoleApp.MongoDbExample
     {
         public async Task Run()
         {
-            var blog = await GetBlogByIdAsync(1);
+            //await ReadById(1);
+            //await Insert(3, "Blog Title 3", "Blog Author 3", "Blog Content 3");
+            await Update(1, "Blog Title 1 updated", "Blog Author 1 updated", "Blog Content 1 updated");
+            await ReadAll();
+        }
+
+        public async Task ReadAll()
+        {
+            var blogs = await GetBlogsAsync();
+            foreach (var blog in blogs)
+            {
+                Console.WriteLine($"Id: {blog.Id}");
+                Console.WriteLine($"Blog Id: {blog.BlogId}");
+                Console.WriteLine($"Blog Title: {blog.BlogTitle}");
+                Console.WriteLine($"Blog Author: {blog.BlogAuthor}");
+                Console.WriteLine($"Blog Content: {blog.BlogContent}");
+                Console.WriteLine("-------------------------------");
+            }
+        }
+
+        public async Task ReadById(int id)
+        {
+            var blog = await GetBlogByIdAsync(id);
             Console.WriteLine($"Id: {blog.Id}");
             Console.WriteLine($"Blog Id: {blog.BlogId}");
             Console.WriteLine($"Blog Title: {blog.BlogTitle}");
             Console.WriteLine($"Blog Author: {blog.BlogAuthor}");
             Console.WriteLine($"Blog Content: {blog.BlogContent}");
+        }
 
-            //AddBlog(3, "Blog Title 3", "Blog Author 3", "Blog Content 3");
+        public async Task Insert(int id, string blogTitle, string blogAuthor, string blogContent)
+        {
+            await AddBlog(id, blogTitle, blogAuthor, blogContent);
+        }
 
-            //var blogs = await GetBlogsAsync();
-            //foreach (var blog in blogs)
-            //{
-            //    Console.WriteLine($"Id: {blog.Id}");
-            //Console.WriteLine($"Blog Id: {blog.BlogId}");
-            //    Console.WriteLine($"Blog Title: {blog.BlogTitle}");
-            //    Console.WriteLine($"Blog Author: {blog.BlogAuthor}");
-            //    Console.WriteLine($"Blog Content: {blog.BlogContent}");
-            //    Console.WriteLine("-------------------------------");
-            //}
+        public async Task Update(int id, string blogTitle, string blogAuthor, string blogContent)
+        {
+            await UpdateBlogAsync(id, blogTitle, blogAuthor, blogContent);
         }
 
         public async Task<List<BlogModel>> GetBlogsAsync()
@@ -49,7 +68,7 @@ namespace DotNet8.ConsoleApp.MongoDbExample
             return blog;
         }
 
-        public void AddBlog(int id, string blogTitle, string blogAuthor, string blogContent)
+        public async Task AddBlog(int id, string blogTitle, string blogAuthor, string blogContent)
         {
             var blog = new BlogModel()
             {
@@ -59,7 +78,24 @@ namespace DotNet8.ConsoleApp.MongoDbExample
                 BlogContent = blogContent
             };
             var collection = GetCollection();
-            collection.InsertOne(blog);
+            await collection.InsertOneAsync(blog);
+        }
+
+        public async Task UpdateBlogAsync(int id, string blogTitle, string blogAuthor, string blogContent)
+        {
+            var blog = await GetBlogByIdAsync(id);
+            ArgumentNullException.ThrowIfNull(blog);
+
+            var collection = GetCollection();
+            var updateDefinition = Builders<BlogModel>.Update
+                .Set(b => b.BlogTitle, blogTitle)
+                .Set(b => b.BlogAuthor, blogAuthor)
+                .Set(b => b.BlogContent, blogContent);
+
+            await collection.UpdateOneAsync(
+                b => b.BlogId == id,
+                updateDefinition
+                );
         }
 
         private IMongoCollection<BlogModel> GetCollection()
