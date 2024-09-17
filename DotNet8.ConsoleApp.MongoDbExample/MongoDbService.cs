a06f060fe5b691e96bbbae717561f6130c6a3a1f
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-17 16:21:34 +0630

    [ Added ] GetBlogByIdAsync

--- DotNet8.ConsoleApp.MongoDbExample/MongoDbService.cs
+++ DotNet8.ConsoleApp.MongoDbExample/MongoDbService.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Driver;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +10,39 @@ namespace DotNet8.ConsoleApp.MongoDbExample
 {
     public class MongoDbService
     {
-        private IMongoCollection<BlogModel> GetCollection()
+        public async Task Run()
         {
-            var client = new MongoClient("mongodb://localhost:27017/");
-            var database = client.GetDatabase("testDb");
-            var collection = database.GetCollection<BlogModel>("Blogs");
+            await GetBlogByIdAsync(1);
 
-            return collection;
+            //AddBlog(3, "Blog Title 3", "Blog Author 3", "Blog Content 3");
+
+            //var blogs = await GetBlogsAsync();
+            //foreach (var blog in blogs)
+            //{
+            //    Console.WriteLine($"Blog Id: {blog.Id}");
+            //    Console.WriteLine($"Blog Title: {blog.BlogTitle}");
+            //    Console.WriteLine($"Blog Author: {blog.BlogAuthor}");
+            //    Console.WriteLine($"Blog Content: {blog.BlogContent}");
+            //    Console.WriteLine("-------------------------------");
+            //}
         }
+
+        public async Task<List<BlogModel>> GetBlogsAsync()
+        {
+            var collection = GetCollection();
+            var blogs = await collection.Find(x => true).ToListAsync();
+
+            return blogs;
+        }
+
+        public async Task<BlogModel> GetBlogByIdAsync(int id)
+        {
+            var collection = GetCollection();
+            var blog = await collection.Find(x => x.BlogId == id).FirstOrDefaultAsync();
+
+            return blog;
+        }
+
         public void AddBlog(int id, string blogTitle, string blogAuthor, string blogContent)
         {
             var blog = new BlogModel()
@@ -30,12 +56,13 @@ namespace DotNet8.ConsoleApp.MongoDbExample
             collection.InsertOne(blog);
         }
 
-        public async Task<List<BlogModel>> GetBlogsAsync()
+        private IMongoCollection<BlogModel> GetCollection()
         {
-            var collection = GetCollection();
-            var blogs = await collection.Find(x => true).ToListAsync();
+            var client = new MongoClient("mongodb://localhost:27017/");
+            var database = client.GetDatabase("testDb");
+            var collection = database.GetCollection<BlogModel>("Blogs");
 
-            return blogs;
+            return collection;
         }
     }
 }
--- DotNet8.ConsoleApp.MongoDbExample/Program.cs
+++ DotNet8.ConsoleApp.MongoDbExample/Program.cs
@@ -8,18 +8,6 @@ public class Program
     {
         MongoDbService dbService = new();
 
-        //dbService.AddBlog(3, "Blog Title 3", "Blog Author 3", "Blog Content 3");
-
-        var blogs = await dbService.GetBlogsAsync();
-        foreach (var blog in blogs)
-        {
-            Console.WriteLine($"Blog Id: {blog.BlogId}");
-            Console.WriteLine($"Blog Title: {blog.BlogTitle}");
-            Console.WriteLine($"Blog Author: {blog.BlogAuthor}");
-            Console.WriteLine($"Blog Content: {blog.BlogContent}");
-            Console.WriteLine("------------------------------");
-        }
-
         Console.ReadKey();
     }
 }
